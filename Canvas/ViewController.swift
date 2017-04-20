f1374eccb30d516332f94aab7cc3a7ab60a0dfa5
Brandon Aubrey <brandonaubrey@brandons-mbp-2>
2017-04-19 19:14:17 -0700

    milestone 5

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -11,13 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
+    
+    var newlyCreatedFace: UIImageView!
     var trayOriginalCenter: CGPoint!
     var trayCenterWhenOpen: CGPoint!
     var trayCenterWhenClosed: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         trayOriginalCenter = trayView.center
+        
+        trayCenterWhenOpen = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y)
+        trayCenterWhenClosed = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + 175)
 
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,6 +33,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTrayTapGesture(_ sender: UITapGestureRecognizer) {
+        if trayView.center.y == trayOriginalCenter.y {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.3, options: [], animations: { self.trayView.center = self.trayCenterWhenClosed }, completion: nil)
+
+        } else {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.3, options: [], animations: { self.trayView.center = self.trayCenterWhenOpen }, completion: nil)
+        }
+    }
+    
 
     @IBAction func onTrayPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
         let translation = panGestureRecognizer.translation(in: view)
@@ -37,16 +53,21 @@ class ViewController: UIViewController {
         if panGestureRecognizer.state == .began {
             
         } else if panGestureRecognizer.state == .changed {
+            
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
         } else if panGestureRecognizer.state == .ended {
+            
             if velocity.y > 0 {
-                trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + 175)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.3, options: [], animations: { self.trayView.center = self.trayCenterWhenClosed }, completion: nil)
                 //moving down
             } else {
-                trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.3, options: [], animations: { self.trayView.center = self.trayCenterWhenOpen }, completion: nil)
                 //moving up
             }
         }
     }
+    
+    @IBAction func onFacePanGesture(_ sender: UIPanGestureRecognizer) {
+        
+    }
 }
-
